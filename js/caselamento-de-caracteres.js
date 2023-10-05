637efc0238d7fb6d23c71ae29066ba66847d339c
JScript <43323432+janielson@users.noreply.github.com>
2023-10-04 21:57:17 -0300

    fiz alguns ajuste no input

--- js/caselamento-de-caracteres.js
+++ js/caselamento-de-caracteres.js
@@ -1,9 +1,17 @@
+let entrada = document.querySelector(".entrada");
 
+entrada.addEventListener("keypress", function (event) {
 
+    let regex = new RegExp('^[0-9]*\\.?[0-9]*$');
+    let key = event.which;
+    let character = String.fromCharCode(key);
 
+    if (!regex.test(character)) {
+        event.preventDefault();
+        
+    }
 
-
-
+});
 
 
 
