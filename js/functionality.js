49effcc472e856da2ccd823fbad11ad286d80ed0
thiagoss2 <thiagojklb@gmail.com>
2023-10-05 13:47:52 -0300

    alterando algumas coias no projeto

--- js/functionality.js
+++ js/functionality.js
@@ -3,9 +3,23 @@ let convertButton = document.querySelector(".button_convert_money");
 
 function convertValues() {
     let valueOfInput = document.querySelector(".entrada").value;
+    let valueScreenBr = document.querySelector(".valuerealbr");
+    let valueScreenUSD = document.querySelector(".valueamerican");
+     
+
     let valueDolarDay = 5.2;
     let valueConvert = valueOfInput / valueDolarDay;
-    console.log(valueOfInput);
+    
+
+    valueScreenBr.innerHTML = new Intl.NumberFormat("pt-br", {
+        style:"currency",
+        currency: "BRL"
+    }).format(valueOfInput)
+
+    valueScreenUSD.innerHTML = new Intl.NumberFormat("en-US",{
+        style: "currency",
+        currency: "USD"
+    }).format(valueConvert)
 }
 
 convertButton.addEventListener("click",convertValues)
