90580408b41188aa114e4575e49223f481e0272d
Thiago Silva <thiagojklb@gmail.com>
2023-10-06 00:14:04 -0300

    fazendo varias mudanças

--- js/functionality.js
+++ js/functionality.js
@@ -1,42 +1,67 @@
 
+
+// aqui na entrada para input não se usa o .value mas sim so o propio elemento
 let convertButton = document.querySelector(".button_convert_money");
-let selectionOfEuroDolar = document.querySelector(".box_select_two");
+let valueOfInput = document.querySelector(".entrada");
+
+
+// aqui não pode selecionar antes da function
+// let selectionOfEuroDolar = document.querySelector(".box_select_two").value;
+
+
+
 
 
 function convertValues() {
 
-    let valueOfInput = document.querySelector(".entrada").value;
-    let valueScreenBr = document.querySelector(".valuerealbr");
-    let valueScreenUSD = document.querySelector(".valueamerican");
+ 
 
-    console.log(selectionOfEuroDolar.value)
+  let selectionOfEuroDolar = document.querySelector(".box_select_two").value;
+  let valueScreenBr = document.querySelector(".valuerealbr");
+  let valueScreenUSD = document.querySelector(".valueamerican");
 
-    let valueDolarDay = 5.17;
-    let valueEuroDay = 4.2
-   
+  let valueDolarDay = 5.17;
+
+  let resultadoDolarParaReal = valueOfInput.value / valueDolarDay;
+  
+  
 
 
-    // new Intl.NumberFormat( "String lingua" ,Objeto estiloTipoMoeda ).format() : void
+  // new Intl.NumberFormat( "String lingua" ,Objeto estiloTipoMoeda ).format() : void
 
-      if(selectionOfEuroDolar == "dolar" ){
-        valueScreenBr.innerHTML = new Intl.NumberFormat("pt-br", {
-            style: "currency",
-            currency: "BRL"
-        }).format(valueOfInput / valueDolarDay)
-
-      }
-      if(selectionOfEuroDolar == "euro" ){
-        valueScreenBr.innerHTML = new Intl.NumberFormat("de-DE", {
-            style: "currency",
-            currency: "EUR"
-        }).format(valueOfInput / valueEuroDay );
-
-      }
 
   
-    
+ 
+  
+  console.log("valor do input " + valueOfInput.value);
+  console.log("Valor do seletor " + selectionOfEuroDolar);
+  console.log("cotação do dolar para real brasileiro " + valueDolarDay);
+  console.log("valor convertido de real para dolar  " + resultadoDolarParaReal);
 
-    
+
+  if (selectionOfEuroDolar === "dolar") { 
+
+    valueScreenBr.innerHTML = new Intl.NumberFormat("pt-br", {
+      style: "currency",
+      currency: "BRL"
+    }).format(resultadoDolarParaReal)
+
+
+
+
+  }
+  if (selectionOfEuroDolar == "euro") {
+
+    valueScreenBr.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR"
+    }).format(valueOfInput / valueEuroDay);
+
+
+
+  }
+
+ 
 
 
 
