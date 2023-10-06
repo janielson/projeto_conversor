08d487503e02eb6456e02dc5e2eeae658019ddfe
JScript <43323432+janielson@users.noreply.github.com>
2023-10-05 22:02:10 -0300

    ateramos o javascript

--- js/functionality.js
+++ js/functionality.js
@@ -1,28 +1,50 @@
+
 let convertButton = document.querySelector(".button_convert_money");
+let selectionOfEuroDolar = document.querySelector(".box_select_two");
 
 
 function convertValues() {
+
     let valueOfInput = document.querySelector(".entrada").value;
     let valueScreenBr = document.querySelector(".valuerealbr");
     let valueScreenUSD = document.querySelector(".valueamerican");
-     
 
-    let valueDolarDay = 5.2;
-    let valueConvert = valueOfInput / valueDolarDay;
+    console.log(selectionOfEuroDolar.value)
+
+    let valueDolarDay = 5.17;
+    let valueEuroDay = 4.2
+   
+
+
+    // new Intl.NumberFormat( "String lingua" ,Objeto estiloTipoMoeda ).format() : void
+
+      if(selectionOfEuroDolar == "dolar" ){
+        valueScreenBr.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL"
+        }).format(valueOfInput / valueDolarDay)
+
+      }
+      if(selectionOfEuroDolar == "euro" ){
+        valueScreenBr.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(valueOfInput / valueEuroDay );
+
+      }
+
+  
     
 
-    valueScreenBr.innerHTML = new Intl.NumberFormat("pt-br", {
-        style:"currency",
-        currency: "BRL"
-    }).format(valueOfInput)
+    
 
-    valueScreenUSD.innerHTML = new Intl.NumberFormat("en-US",{
-        style: "currency",
-        currency: "USD"
-    }).format(valueConvert)
+
+
 }
 
-convertButton.addEventListener("click",convertValues)
+convertButton.addEventListener("click", convertValues);
+
+
 
 
 
